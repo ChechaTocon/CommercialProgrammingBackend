fbfd373d4d5dcb995bcfa50f247ea6f5ffa10ae8
Celeste Galicia <celes0_33@hotmail.es>
2020-12-14 16:31:48 -0600

    Pruebas

--- reviews/tests/test_models.py
+++ reviews/tests/test_models.py
@@ -1,16 +1,37 @@
 from django.contrib.auth.models import User
 from django.test import TestCase
-from reviews.models import Category
+from reviews.models import Category, Movie
 
-class CategoryModelTest(TestCase):    
+class ModelTest(TestCase):    
 
     @classmethod
     def setUpTestData(cls):
         category = Category.objects.create(name = 'Terror',color= '#ffffff')
+        Movie.objects.create(poster = 'https://emmawatsonperu.org/wp-content/uploads/2017/01/News2017-10.jpg',movieName= 'La bella y la bestia',description="Es una pelicula de disney", category=category)
         pass
-
+        #test para categoria
     def test_color_max_length(self):
         category=Category.objects.get(id=1)
         max_length = Category._meta.get_field('color').max_length
         self.assertEquals(max_length,20)
+
+    def test_name_max_length(self):
+        category=Category.objects.get(id=1)
+        max_length = Category._meta.get_field('name').max_length
+        self.assertEquals(max_length,20)
+
+         #test para pelicula
+    def test_movieName_max_length(self):
+        movie=Movie.objects.get(id=1)
+        max_length = Movie._meta.get_field('movieName').max_length
+        self.assertEquals(max_length,25)
+
+    def test_description_max_length(self):
+        movie=Movie.objects.get(id=1)
+        max_length = Movie._meta.get_field('description').max_length
+        self.assertEquals(max_length,255)   
+
+   
+    
+
 
